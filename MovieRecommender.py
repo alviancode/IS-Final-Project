85167860867a87dc6f92375d0abfff4b15c7d648
alviancode <alvianwijaya200@gmail.com>
2021-06-16 20:27:38 +0700

    add comment

--- MovieRecommender.py
+++ MovieRecommender.py
@@ -3,40 +3,94 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from ast import literal_eval
-#from fuzzywuzzy import process
 import omdb
 
-#omdb.set_default('apikey', 'API KEY')
+# Read the dataset.
+movies = pd.read_csv("movie_dataset.csv")
+
+# OMDB API
+omdb.set_default('apikey', 'a3e014fb')
 omdbRes=omdb.search('True Grit')[0].get('poster')
 
+# Function to return movie's poster from IMDB using OMDB API.
 def getPoster(title):
     try:
-        #return 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
-        return omdb.search(title)[0].get('poster')
+        return ''
+        #return omdb.search(title)[0].get('poster')
     except:
         return 'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
 
-def get_title_from_index(index):
+# This function return the movie title from a given index.
+def getTitleFromIndex(index):
 	return movies[movies.index == index]["title"].values[0]
 
+# This function return the date of the movie from a given index.
 def getDate(index):
 	return movies[movies.index == index]["release_date"].values[0]
 
-def get_index_from_title(title):
+# This function return the index of the movie from a given title.
+def getIndexFromTitle(title):
     return movies[movies.title == title]["index"].values[0]
 ##################################################
 
-movies = pd.read_csv("movie_dataset.csv")
 
+
+# Create a combination of features from the movie.
 def combineFeatures(row):
 	try:
 		return row['keywords'] +" "+row['cast']+" "+row["genres"]+" "+row["director"]
 	except:
 		print ("Error:", row)	
 
+# Function that recommend the movie.
+def recommendations(movieTitle, boolean = False):
+    features = ['keywords','cast','genres','director', 'crew']
+    result = []
+    
+    # Create a column in the DataFram that store the combined features.
+    for feature in features:
+        movies[feature] = movies[feature].fillna('')
+    movies["combinedFeatures"] = movies.apply(combineFeatures, axis=1)
 
+    # Count word frequency
+    countVec = CountVectorizer(stop_words = 'english')
+    countMatrix = countVec.fit_transform(movies["combinedFeatures"])
+    
+    # Convert Sparse Matrix to DataFrame
+    termMatrix = countMatrix.todense()
+    dataFrame = pd.DataFrame(termMatrix, columns= countVec.get_feature_names())
+    
+    # Calculating cosine similarities
+    cosineSim = pd.DataFrame(cosine_similarity(dataFrame, dense_output=True))
+    
+    movieUserLike = movieTitle
 
+    movie_index = getIndexFromTitle(movieUserLike)
+
+    # Calculate the similar movies.
+    similarMovies =  list(enumerate(cosineSim[movie_index]))
+
+    # Sorted the similar movie by the similarity value.
+    sortedSimilarMovies = sorted(similarMovies,key=lambda x:x[1],reverse=True)
+    
+    i=0
+    for element in sortedSimilarMovies:
+        result.append({
+            "title" : getTitleFromIndex(element[0]),
+            "index" :element[0],
+            "date" : getDate(element[0]),
+            "poster" : getPoster(getTitleFromIndex(element[0]))
+                       })
+        i=i+1
+        if i>30:
+            return result
+        
+    
+
+# Function for fuzzy serach.
 def levenshteinDistance(a:str,b:str) -> int:
+    
+    # Filter the input title and also the dataset titles for better result.
     a, b = a.lower(), b.lower()
     punctuation = '''!()-[]{};:'"\, <>./?@#$%^&*_~ '''
     for element in a:
@@ -46,22 +100,28 @@ def levenshteinDistance(a:str,b:str) -> int:
         if element in punctuation:
             b = b.replace(element, '')
     
+    
     lenA = len(a)
     lenB = len(b)
-    d2Memo = [[0 for i in range(lenB+1)] for k in range(lenA+1)]
+    d = [[0 for i in range(lenB+1)] for k in range(lenA+1)]
+        
     for i in range(1,lenA+1):
         for m in range(1,lenB+1):
-            k = 0 if a[i-1] == b[m-1] else 2
-            d2Memo[i][m] = min(d2Memo[i-1][m] + 1 ,
-                                d2Memo[i][m-1] + 1 ,
-                                d2Memo[i-1][m-1] + k
+            if a[i-1] == b[m-1]:
+                cost = 0
+            else:
+                cost = 1
+
+            d[i][m] = min(d[i-1][m] + 1 ,     # deletion.
+                                d[i][m-1] + 1 ,    # insertion.
+                                d[i-1][m-1] + cost    # substitution.
                                 )
-    return d2Memo[lenA][lenB]
+    return d[lenA][lenB]
 
 
 
 
-
+# Function that return the best rating movies.
 def bestMovies(items = 20):
     #print(movieSort[['title', 'index', "vote_average"]])
     array = []
@@ -84,15 +144,10 @@ def bestMovies(items = 20):
                       "poster" : getPoster(temp.values.tolist()[i][0])
                       })
     return array
-    
 
 
 
-#print(bestMovies())
-
-
-
-
+# Helper function for fuzzy search for REST API purposes.
 def fuzzySearch(title, items = 10):
     index=[]
     temp =[]
@@ -102,67 +157,13 @@ def fuzzySearch(title, items = 10):
         j=j+1
     #return index
     for i in range(items):
-        temp.append({"title":get_title_from_index(sorted(index)[i][1]),
+        temp.append({"title":getTitleFromIndex(sorted(index)[i][1]),
                      "index":sorted(index)[i][1], 
                      "date":getDate(sorted(index)[i][1]),
-                     "poster":getPoster(get_title_from_index(sorted(index)[i][1]))
+                     "poster":getPoster(getTitleFromIndex(sorted(index)[i][1]))
                      })
-        
     return temp
 
 
-#print(process.extract("ironman", movies['title']))
 
-
-#print("\nUSING DIY:")
-#print(fuzzySearch("ironman"))
-
-
-def recommendations(movieTitle, boolean = False):
-    features = ['keywords','cast','genres','director', 'crew']
-    result = []
-    
-    for feature in features:
-        movies[feature] = movies[feature].fillna('')
-    
-    movies["combinedFeatures"] = movies.apply(combineFeatures, axis=1)
-
-    #Count word frequency
-    countVec = CountVectorizer(stop_words = 'english')
-    countVec = CountVectorizer()
-    countMatrix = countVec.fit_transform(movies["combinedFeatures"])
-    
-    #Convert Sparse Matrix to DataFrame
-    termMatrix = countMatrix.todense()
-    dataFrame = pd.DataFrame(termMatrix, columns= countVec.get_feature_names())
-    
-    #Calculating cosine similarities
-    cosineSim = pd.DataFrame(cosine_similarity(dataFrame, dense_output=True))
-    
-    
-    movie_user_likes = movieTitle
-
-    movie_index = get_index_from_title(movie_user_likes)
-
-    similar_movies =  list(enumerate(cosineSim[movie_index]))
-
-
-    sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)
-    
-    i=0
-    for element in sorted_similar_movies:
-        result.append({
-            "title" : get_title_from_index(element[0]),
-            "index" :element[0],
-            "date" : getDate(element[0]),
-            "poster" : getPoster(get_title_from_index(element[0]))
-                       })
-        i=i+1
-        if i>30:
-            return result
-        
-    
-
-
-
-#print(recommendations("Iron Man"))
+print(fuzzySearch("Iron Man"))
